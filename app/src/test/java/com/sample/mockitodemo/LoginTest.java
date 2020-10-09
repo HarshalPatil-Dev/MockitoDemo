b6b8c1e582fa038da1b5833fe896b9bd032aac3b
harshal <NA>
2020-10-09 21:30:49 +0700

    Test Changes Added.

--- app/src/test/java/com/sample/mockitodemo/LoginTest.java
+++ app/src/test/java/com/sample/mockitodemo/LoginTest.java
@@ -1,17 +1,11 @@
 package com.sample.mockitodemo;
 
-import com.sample.mockitodemo.Login.Login;
 import com.sample.mockitodemo.Login.LoginPresenter;
 import com.sample.mockitodemo.Login.LoginPresenterImpl;
 import com.sample.mockitodemo.Login.LoginView;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -32,23 +26,22 @@ public class LoginTest {
         when(loginView.getUserName()).thenReturn("");
         when(loginView.getPassword()).thenReturn("HarshalPatil");
         SUT.onLoginClick();
-        assertThat(loginView.getUserName(), is(""));
-        assertThat(loginView.getPassword(), is("HarshalPatil"));
+        verify(loginView).showMessage(R.string.error_empty_username);
+
     }
     @Test
     public void Empty_PasswordTest(){
         when(loginView.getUserName()).thenReturn("Harshal");
         when(loginView.getPassword()).thenReturn("");
         SUT.onLoginClick();
-        assertThat(loginView.getUserName(),is("Harshal"));
-        assertThat(loginView.getPassword(),is(""));
+        verify(loginView).showMessage(R.string.error_empty_password);
     }
     @Test
     public void Success_PasswordTest(){
         when(loginView.getUserName()).thenReturn("Harshal");
         when(loginView.getPassword()).thenReturn("Patil");
         SUT.onLoginClick();
-        assertThat(loginView.getUserName(),is("Harshal"));
-        assertThat(loginView.getPassword(),is("Patil"));
+        verify(loginView).showMessage(R.string.success_login);
+
     }
 }
