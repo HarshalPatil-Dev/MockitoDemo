15a2f3b2ad4002205775600f00711b02c7c58e63
harshal <NA>
2020-10-10 10:46:48 +0700

    Test Changes Added.

--- app/src/test/java/com/sample/mockitodemo/LoginTest.java
+++ app/src/test/java/com/sample/mockitodemo/LoginTest.java
@@ -4,9 +4,14 @@ import com.sample.mockitodemo.Login.LoginPresenter;
 import com.sample.mockitodemo.Login.LoginPresenterImpl;
 import com.sample.mockitodemo.Login.LoginView;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -23,25 +28,31 @@ public class LoginTest {
 
     @Test
     public void Empty_UserNameTest(){
+        ArgumentCaptor<Integer> ac = ArgumentCaptor.forClass(Integer.class);
         when(loginView.getUserName()).thenReturn("");
         when(loginView.getPassword()).thenReturn("HarshalPatil");
         SUT.onLoginClick();
-        verify(loginView).showMessage(R.string.error_empty_username);
+        verify(loginView,times(1)).showMessage(ac.capture());
+        Assert.assertThat(ac.getValue(),is(R.string.error_empty_username));
 
     }
     @Test
     public void Empty_PasswordTest(){
+        ArgumentCaptor<Integer> ac = ArgumentCaptor.forClass(Integer.class);
         when(loginView.getUserName()).thenReturn("Harshal");
         when(loginView.getPassword()).thenReturn("");
         SUT.onLoginClick();
-        verify(loginView).showMessage(R.string.error_empty_password);
+        verify(loginView,times(1)).showMessage(ac.capture());
+        Assert.assertThat(ac.getValue(),is(R.string.error_empty_password));
     }
     @Test
     public void Success_PasswordTest(){
+        ArgumentCaptor<Integer> ac = ArgumentCaptor.forClass(Integer.class);
         when(loginView.getUserName()).thenReturn("Harshal");
         when(loginView.getPassword()).thenReturn("Patil");
         SUT.onLoginClick();
-        verify(loginView).showMessage(R.string.success_login);
+        verify(loginView,times(1)).showMessage(ac.capture());
+        Assert.assertThat(ac.getValue(),is(R.string.success_login));
 
     }
 }
